d2769323ce6b1f09fe66d98e2095efa61a5ec794
rizwanahmed1981 <razykan.ra@gmail.com>
2025-02-28 11:13:04 +0500

    issues with files uploading resolved

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if uploaded_files:
 
 
         if file_ext == ".csv":
-            df = pd.read_csv(file)
+            df = pd.read_csv(file, encoding="ISO-8859-1")
         elif file_ext == ".xlsx":
             try:
                 df = pd.read_excel(file, engine='openpyxl')
